b1e502c1bc336a76a3f2b262502a57a257e5ee38
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-18 18:44:05 -0300

    Style: estilização da pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,35 +1,72 @@
+import * as zod from 'zod'
+
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import {
     Container,
     FormContainer,
-    InputContainer
+    InputContainer, 
+    NoCount, 
+    ButtonLogin
 } from './styles'
+
+const loginValidationSchema = zod.object({
+    email: zod.string(),
+    password: zod.string()
+})
+
+
+export type NewLoginData = zod.infer<typeof loginValidationSchema>
+
 
 export function Login(){
 
+    const {
+        register,
+        handleSubmit,
+      } = useForm<NewLoginData>({
+        resolver: zodResolver(loginValidationSchema),
+      });
+
+    function handleLogin(data: NewLoginData) {
+        console.log('login')
+    }
+
     return (
         <Container>
-             <FormContainer>
+             <FormContainer onSubmit={handleSubmit(handleLogin)}>
                 <span>Faça login</span>
 
                 <InputContainer>
                     <input
                         placeholder='E-mail'
+                        {...register('email')}
+                        required
                     />
 
                     <input
                         placeholder='senha'
+                        {...register('password')}
+                        required
                     />
                 </InputContainer>
                
 
-                <button>Entrar</button>
+                <ButtonLogin type='submit'>
+                    Entrar
+                </ButtonLogin>
              </FormContainer>
              
-             <div>
+             <NoCount>
                 <span>Não tenha conta?</span>
-                <button>Registre-se</button>
-             </div>
+
+                <p>
+                    <a href="">
+                        Registre-se
+                    </a>
+                </p>
+             </NoCount>
         </Container>
     )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -7,24 +7,61 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     
-    
-    height: 80vh;
+    height: 90vh;
     
     
     
 `
-export const FormContainer = styled.div`
+export const FormContainer = styled.form`
     display: flex;
     flex-direction: column;
+    align-items: center;
     justify-content: center;
     gap: 0.5rem;
+
+    padding: 1rem;
+    box-shadow: 3px 3px 0px 0px rgba(0,0,0,0.75);
+    border-radius: 8px;
+    border: 1px solid;
    
-   
+    
 `
 export const InputContainer = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     justify-content: center;
     gap: 0.5rem;
+
+    input{
+        width: 15rem;
+        height: 2rem;
+    }
+
+   
+`
+
+export const ButtonLogin = styled.button`
+    width: 100px;
+    height: 30px;
+
+    background-color: #24f005;
+    border: 0px;
+    border-radius: 4px;
+
+    cursor: pointer;
+`
+
+export const NoCount = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.2rem;
+
+    a{
+        text-decoration: none;
+        color: black;
+        font-weight: bold;
+    }
 
 `
