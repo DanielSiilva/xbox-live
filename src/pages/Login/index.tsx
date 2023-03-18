057e8230dc4618de145cef2ea535d18bdaaad2b6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-18 16:50:46 -0300

    Feat: estrutura e styles da pagina home

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,35 @@
 
+import {
+    Container,
+    FormContainer,
+    InputContainer
+} from './styles'
 
 export function Login(){
 
     return (
-        <>
-             <h1> Login</h1>
-        </>
+        <Container>
+             <FormContainer>
+                <span>Faça login</span>
+
+                <InputContainer>
+                    <input
+                        placeholder='E-mail'
+                    />
+
+                    <input
+                        placeholder='senha'
+                    />
+                </InputContainer>
+               
+
+                <button>Entrar</button>
+             </FormContainer>
+             
+             <div>
+                <span>Não tenha conta?</span>
+                <button>Registre-se</button>
+             </div>
+        </Container>
     )
 }
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -2,5 +2,29 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    
+    
+    height: 80vh;
+    
+    
+    
+`
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5rem;
+   
+   
+`
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 0.5rem;
 
 `
