05b8c0ce4e9b5884f190112ae154e224149e62ac
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-03-18 21:52:05 -0300

    Bug: bug na criação de um novo usuario

--- src/pages/CreateUser/index.tsx
+++ src/pages/CreateUser/index.tsx
@@ -1,3 +1,11 @@
+import * as zod from 'zod'
+
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { api } from '../../lib/axios'
+
+
 
 import {
     Container,
@@ -8,35 +16,72 @@ import {
 
 } from './styles'
 
+import { useNavigate } from 'react-router-dom'
+
+const createUserValidationSchema = zod.object({
+    name: zod.string(),
+    email: zod.string(),
+    password: zod.string(),
+    cpf: zod.number(),
+})
+
+
+export type CreateUserData = zod.infer<typeof createUserValidationSchema>
 
 
 
 export function CreateUse(){
+    const navigate = useNavigate()
+
+    const {
+        register,
+        handleSubmit,
+        reset
+      } = useForm<CreateUserData>({
+        resolver: zodResolver(createUserValidationSchema),
+      });
+
+
+      async function handleCreateUser(data: CreateUserData) {
+        console.log(data);
+
+        navigate('/')
+    }
+
+    function gotoPageLogin(){
+        navigate('/')
+    }
+
 
     return(
         <Container>
-            <FormContainer>
+            <FormContainer onSubmit={handleSubmit(handleCreateUser)}>
                 <input
                     placeholder='Digite seu nome'
                     required
+                    {...register('name')}
                 />
 
                 <input
                     placeholder='Digite um email'
                     required
+                    {...register('email')}
                 />
 
                 <input
                     placeholder='Digite uma senha'
                     required
+                    {...register('password')}
                 />
 
                 <input
                     placeholder='Digite seu CPF'
                     required
+                    {...register('cpf', {valueAsNumber: true})}
+                      
                 />
 
-                <ButtonCreate>
+                <ButtonCreate type='submit' >
                     inscreva-se
                 </ButtonCreate>
             </FormContainer>
@@ -44,7 +89,7 @@ export function CreateUse(){
             <NoCount>
                 já tem uma conta?
                     
-                <ButtonLogin>
+                <ButtonLogin onClick={gotoPageLogin}>
                     entre
                 </ButtonLogin>
             </NoCount>
